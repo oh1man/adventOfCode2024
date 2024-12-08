eb3085132e320a12118f5b06409c75ad199a839f
Adam <adam.ohman@vermiculus.se>
2024-12-08 11:20:24 +0100

    Day 4

--- day4/index.ts
+++ day4/index.ts
@@ -1,26 +1,62 @@
-import {createMatrix, readFileToString} from "../utils/FileUtilities.ts";
-import {Direction, Scan2D} from "./scanning.ts";
-
-function includes(arg0: string, scan: string[]) {
-    const word = scan.join("");
-    const drow = scan.reverse().join("");
-    return arg0 === word || arg0 === drow
-}
+import { createMatrix, readFileToString } from "../utils/FileUtilities.ts";
+import { Horizontal, Scan2D, Vertical } from "./scanning.ts";
 
 const input = await readFileToString("./day4/input.txt");
 const matrix = createMatrix(input);
 
-let setOfCoordinates = new Set<[][]>();
-const scanner = new Scan2D(matrix);
-for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix[row].length; col++) {
-        for (const direction of Object.values(Direction)) {
-            const [scan, coordinates] = scanner.getOneDimensionalScan(col, row, direction as Direction, 4);
-            if (includes("XMAS", scan)) {
-                setOfCoordinates.add(coordinates)
+// Part 1
+{
+    const scanner = new Scan2D(matrix);
+    let numberOfXmas = 0;
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            const val = matrix[row][col];
+            if (val !== "X") {
+                continue;
+            }
+            for (const horizontal of Object.values(Horizontal)) {
+                for (const vertical of Object.values(Vertical)) {
+                    const [scan, _] = scanner.getOneDirectionalScan(
+                        col,
+                        row,
+                        horizontal as Horizontal,
+                        vertical as Vertical,
+                        4,
+                    );
+                    if ("XMAS" === scan.join("")) {
+                        numberOfXmas += 1;
+                    }
+                }
             }
         }
     }
+    console.log(numberOfXmas);
 }
-console.log(setOfCoordinates.size);
 
+{
+    const scanner = new Scan2D(matrix);
+    let numberOfXmas = 0;
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            const val = matrix[row][col];
+            if (val !== "A") {
+                continue;
+            }
+            const [diag1, diag2] = scanner.getDiagonals(
+                col,
+                row,
+                1,
+            );
+            if (isMas(diag1) && isMas(diag2)) {
+                numberOfXmas++;
+            }
+        }
+    }
+    console.log(numberOfXmas);
+}
+
+function isMas(diag1: string[]): boolean{
+    const word = diag1.join("");
+    const reverse = diag1.reverse().join("");
+    return  "MAS" == word || "MAS" === reverse;
+}
--- day4/scanning.ts
+++ day4/scanning.ts
@@ -15,49 +15,104 @@ export class Scan2D {
      * @param direction The direction we want to return a list of elements from.
      * @param numberOfElements The number of elements we want to include in the scan.
      */
-    public getOneDimensionalScan(
+    public getOneDirectionalScan(
         x: number,
         y: number,
-        direction: Direction,
+        horizontal: Horizontal,
+        vertical: Vertical,
         numberOfElements: number,
-    ): any[] {
+    ): [string[], number[][]] {
         const scan: any[] = [];
         const coordinates: number[][] = [];
         for (let i = 0; i < numberOfElements; i++) {
-            let _x = x;
-            let _y = y;
-            switch (direction) {
-                case Direction.HORIZONTAL:
-                    _x = x + i;
-                    break;
-                case Direction.VERTICAL:
-                    _y = y + i;
-                    break;
-                case Direction.RIGHT_DIAGONAL:
-                    _x = x + i;
-                    _y = y + i;
-                    break;
-                case Direction.LEFT_DIAGONAL:
-                    _x = x - i;
-                    _y = y + i;
-                    break;
-                default:
-                    throw new Error("Invalid direction direction");
-            }
+            const [_x, _y] = getDirectionalCoordinate(
+                horizontal,
+                vertical,
+                x,
+                y,
+                i,
+            );
             try {
-                scan.push(this.input[_x][_y]);
-                coordinates.push([_x, _y])
+                scan.push(this.input[_y][_x]);
+                coordinates.push([_x, _y]);
             } catch (e) {
                 //console.log("Out of bound")
             }
         }
         return [scan, coordinates];
     }
+
+    public getDiagonals(
+        x: number,
+        y: number,
+        numberOfElementsFromMidPoint: number,
+    ) {
+        const diagonal1: string[] = [];
+        const coordinates1: number[][] = [];
+        const diagonal2: string[] = [];
+        const coordinates2: number[][] = [];
+        for (let i = -numberOfElementsFromMidPoint; i <= numberOfElementsFromMidPoint; i++) {
+            const _x1 = x + i;
+            const _y1 = y - i;
+            try {
+                diagonal1.push(this.input[_y1][_x1]);
+                coordinates1.push([_x1, _y1]);
+            } catch (e) {}
+            const _x2 = x + i;
+            const _y2 = y + i;
+            try {
+                diagonal2.push(this.input[_y2][_x2]);
+                coordinates2.push([_x2, _y2]);
+            } catch (e) {}
+        }
+        return [diagonal1, diagonal2];
+    }
 }
 
-export enum Direction {
-    HORIZONTAL = "HORIZONTAL",
-    VERTICAL = "VERTICAL",
-    RIGHT_DIAGONAL = "RIGHT_DIAGONAL",
-    LEFT_DIAGONAL = "LEFT_DIAGONAL",
+export enum Vertical {
+    UP = "UP",
+    DOWN = "DOWN",
+    NONE = "NONE",
+}
+
+export enum Horizontal {
+    RIGHT = "RIGHT",
+    LEFT = "LEFT",
+    NONE = "NONE",
+}
+
+export function getDirectionalCoordinate(
+    horizontal: Horizontal,
+    vertical: Vertical,
+    x: number,
+    y: number,
+    stepSize: number = 1,
+) {
+    return [getX(horizontal, x, stepSize), getY(vertical, y, stepSize)];
+}
+
+function getX(horizontal: Horizontal, x: number, stepSize: number) {
+    switch (horizontal) {
+        case Horizontal.RIGHT:
+            return x + stepSize;
+        case Horizontal.LEFT:
+            return x - stepSize;
+        case Horizontal.NONE:
+            return x;
+        default:
+            throw new Error("Invalid horizontal direction");
+    }
+}
+
+function getY(vertical: Vertical, y: number, stepSize: number) {
+    switch (vertical) {
+        case Vertical.UP:
+            return y - stepSize;
+        case Vertical.DOWN:
+            return y + stepSize;
+        case Vertical.NONE:
+            return y;
+        default:
+            throw new Error("Invalid vertical direction");
+    }
 }
